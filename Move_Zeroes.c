039c79be236bfb5a85be4d85081f7e34e30e7e3f
21A91A05E6 <97424463+21A91A05E6@users.noreply.github.com>
2022-03-12 11:37:22 +0530

    #include<stdio.h> int main() {     int x[20],i,n,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         if(x[i]!=0)         {             x[j]=x[i];             j++;         }     }     for(i=j;i<n;i++)     {         x[i]=0;     }     for(i=0;i<n;i++)     {         printf("%d ",x[i]);     }     return 0; }

---
+++ Move_Zeroes.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int x[20],i,n,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(x[i]!=0)
+        {
+            x[j]=x[i];
+            j++;
+        }
+    }
+    for(i=j;i<n;i++)
+    {
+        x[i]=0;
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",x[i]);
+    }
+    return 0;
+}
