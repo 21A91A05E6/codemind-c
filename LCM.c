0420dc9079738b655309f58150db889a8304283a
21A91A05E6 <97424463+21A91A05E6@users.noreply.github.com>
2022-04-18 19:44:31 +0530

    #include <stdio.h>
    // A recursive function that returns the GCD of two numbers.
    int gcd(int x, int y)
    {
    	return y == 0 ? x : gcd(y, x % y);
    }
    
    int lcm(int x, int y)
    {
    	return x * y / gcd(x, y);
    }
    
    int main()
    {
    	int a, b;
    	scanf("%d %d", &a, &b);
    	printf("%d",lcm(a, b));
    }

--- LCM.c
+++ LCM.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-int main() {
-    int n1, n2, max;
- 
-    scanf("%d %d", &n1, &n2);
 
-    // maximum number between n1 and n2 is stored in max
-    max = (n1 > n2) ? n1 : n2;
+// A recursive function that returns the GCD of two numbers.
+int gcd(int x, int y)
+{
+	return y == 0 ? x : gcd(y, x % y);
+}
 
-    while (1) {
-        if (max % n1 == 0 && max % n2 == 0) {
-            printf("%d", max);
-            break;
-        }
-        ++max;
-    }
-    return 0;
+int lcm(int x, int y)
+{
+	return x * y / gcd(x, y);
+}
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d",lcm(a, b));
 }
