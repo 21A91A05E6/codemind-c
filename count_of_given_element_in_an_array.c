7ba420f24c02c8bdb3004a213bcc035149e8d889
21A91A05E6 <97424463+21A91A05E6@users.noreply.github.com>
2022-04-19 15:53:24 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,k,dc=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(k==arr[i])         {             dc++;         }     }     printf("%d",dc); }

---
+++ count_of_given_element_in_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,k,dc=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(k==arr[i])
+        {
+            dc++;
+        }
+    }
+    printf("%d",dc);
+}
