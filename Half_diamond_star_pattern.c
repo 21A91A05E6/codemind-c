6c5cc3905e5e448c848052590983f8f33f45327e
21A91A05E6 <97424463+21A91A05E6@users.noreply.github.com>
2022-04-21 20:45:46 +0530

    #include<stdio.h> int main() {  	int i, j, rows;  	scanf("%d", &rows);     if(rows>=3)     { 	for(i = 1; i <= rows; i++) 	{         for(j = 1; j <= i; j++)         {              printf("*");         }         printf(" ");     }     for(i = rows - 1; i > 0; i--) 	{         for(j = 1; j <= i; j++)         {              printf("*");         }         printf(" ");     }     }     else     {         printf("The pattern is not possible");     }  	return 0; }

---
+++ Half_diamond_star_pattern.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+ 	int i, j, rows; 
+ 	scanf("%d", &rows);
+    if(rows>=3)
+    {
+	for(i = 1; i <= rows; i++)
+	{
+        for(j = 1; j <= i; j++)
+        {
+             printf("*");
+        }
+        printf("
+");
+    }
+    for(i = rows - 1; i > 0; i--)
+	{
+        for(j = 1; j <= i; j++)
+        {
+             printf("*");
+        }
+        printf("
+");
+    }
+    }
+    else
+    {
+        printf("The pattern is not possible");
+    }
+ 	return 0;
+}
