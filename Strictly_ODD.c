a5efa7129ba33d6d4cfa94a0b23eed6ea1545daf
21A91A05E6 <97424463+21A91A05E6@users.noreply.github.com>
2022-06-29 21:28:49 +0530

    #include<stdio.h> int main() {     int arr[100],i,n,dc=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2 && i%2)         {           continue;         }         if(i%2==0 && arr[i]%2)         {             dc++;         }     }     if(dc==0)     {         printf("True");     }     else     {         printf("False");     } }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,27 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    int x[20],n,i,flag=0;
+    int arr[100],i,n,dc=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&x[i]);
+        scanf("%d",&arr[i]);
     }
-     for(i=0;i<n;i++)
-     {
-         if(x[i]%2!=0)
-         {
-             if(i%2==0)
-             {
-                 flag=1;
-                 break;
-             }
-         }
-     }
-     if(flag==0)
-     {
-         printf("True");
-     }
-     else
-     printf("False");
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2 && i%2)
+        {
+          continue;
+        }
+        if(i%2==0 && arr[i]%2)
+        {
+            dc++;
+        }
+    }
+    if(dc==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
 }
