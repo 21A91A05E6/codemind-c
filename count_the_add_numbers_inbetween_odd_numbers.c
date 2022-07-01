d8d016216160c16a0fbd0b0915e597568e399d21
21A91A05E6 <97424463+21A91A05E6@users.noreply.github.com>
2022-07-01 10:21:36 +0530

    #include<stdio.h> int main() {     int i,arr[100],n,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if((arr[i-1]%2!=0 && arr[i+1]%2!=0) && arr[i]%2!=0)         {             c+=1;         }     }     printf("%d",c); }

--- count_the_add_numbers_inbetween_odd_numbers.c
+++ count_the_add_numbers_inbetween_odd_numbers.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int arr[100],n,i,dc=0;
+    int i,arr[100],n,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=1;i<n-1;i++)
+    for(i=0;i<n;i++)
     {
-        if(arr[i-1]%2 && arr[i+1]%2 && arr[i]%2)
+        if((arr[i-1]%2!=0 && arr[i+1]%2!=0) && arr[i]%2!=0)
         {
-            dc++;
+            c+=1;
         }
     }
-    printf("%d",dc);
+    printf("%d",c);
 }
