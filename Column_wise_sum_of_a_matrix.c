4cb263611868abc3256860f4291e2b98151e3e7a
21A91A05E6 <97424463+21A91A05E6@users.noreply.github.com>
2022-07-11 11:02:00 +0530

    #include <stdio.h> int main() {      int a[100][100],i,n,m,j,sum=0,max=0;      scanf("%d%d",&n,&m);      for(i=0;i<n;i++)      {           for(j=0;j<m;j++)           {                scanf("%d",&a[i][j]);           }      }      for(j=0;j<m;j++)      {         sum=0;         for(i=0;i<n;i++)         {             sum=sum+a[i][j];         }         printf("%d ",sum);      }
    }

--- Column_wise_sum_of_a_matrix.c
+++ Column_wise_sum_of_a_matrix.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-
 int main()
 {
-    int i, j, m, n;
-    int matrix[100][100];
-    int sumR, sumC;
-    scanf("%d", &m);
-    scanf("%d", &n);
-
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)
+     int a[100][100],i,n,m,j,sum=0,max=0;
+     scanf("%d%d",&n,&m);
+     for(i=0;i<n;i++)
+     {
+          for(j=0;j<m;j++)
+          {
+               scanf("%d",&a[i][j]);
+          }
+     }      
+     for(j=0;j<m;j++)
+     {
+        sum=0;
+        for(i=0;i<n;i++)
         {
-            scanf("%d", &matrix[i][j]);
+            sum=sum+a[i][j]; 
         }
-    }
-
-    for (i = 0; i < n; i++)
-    {
-        sumC = 0;
-        for (j = 0; j < m; j++)
-        {
-            sumC += matrix[j][i];
-        }
-        printf("%d ",sumC);
-    }
-
+        printf("%d ",sum);
+     } 
+     
 }
