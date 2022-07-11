c1ca717ab6c56f979398ed9d38760958fa331fa9
21A91A05E6 <97424463+21A91A05E6@users.noreply.github.com>
2022-07-11 11:11:36 +0530

    #include <stdio.h> int main() {      int a[100][100],i,n,m,j,sum=0,max=0,a1;      scanf("%d%d",&n,&m);      for(i=0;i<n;i++)      {           for(j=0;j<m;j++)           {                scanf("%d",&a[i][j]);           }      }      for(i=0;i<n;i++)      {
            for(j=0;j<m;j++)
            {
                max=max+a[i][j];
                if((i==0) || (i==n-1) || (j==0) || (j==m-1))
                {
                    sum=sum+a[i][j]; 
                }
            }
            
         } 
         a1=max-sum;
         printf("%d ",a1);
    }

---
+++ Sum_of_non_boundary_values_in_a_matrix.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+     int a[100][100],i,n,m,j,sum=0,max=0,a1;
+     scanf("%d%d",&n,&m);
+     for(i=0;i<n;i++)
+     {
+          for(j=0;j<m;j++)
+          {
+               scanf("%d",&a[i][j]);
+          }
+     }      
+     for(i=0;i<n;i++)
+     {
+        
+        for(j=0;j<m;j++)
+        {
+            max=max+a[i][j];
+            if((i==0) || (i==n-1) || (j==0) || (j==m-1))
+            {
+                sum=sum+a[i][j]; 
+            }
+        }
+        
+     } 
+     a1=max-sum;
+     printf("%d ",a1);
+}
